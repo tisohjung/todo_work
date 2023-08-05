909fad517d461de16e794b391ffb570f66504473
Minho Yi <tisohjung@gmail.com>
2023-08-05 16:00:19 +0900

    Todo Service 임시 메모리에 작성

---
+++ lib/services/todo_service.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todo_work/model/todo.dart';
+
+class TodoService with ChangeNotifier {
+  final List<Todo> _todos = [];
+
+  Future<List<Todo>> getTodoList() async {
+    return Future.delayed(const Duration(milliseconds: 300), () => _todos);
+  }
+
+  addTodo(Todo todo) async {
+    Future.delayed(const Duration(milliseconds: 300), () {
+      _todos.add(todo);
+      notifyListeners();
+    });
+  }
+
+  removeTodo(String id) async {
+    Future.delayed(const Duration(milliseconds: 300), () {
+      _todos.removeWhere((element) => element.id == id);
+      notifyListeners();
+    });
+  }
+}
