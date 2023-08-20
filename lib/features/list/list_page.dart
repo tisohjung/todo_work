de2760467b5391194445e07d9c1587fb16bf3dba
Minho Yi <tisohjung@gmail.com>
2023-08-21 02:17:21 +0900

    🚩 use hive to listen to todolist

--- lib/features/list/list_page.dart
+++ lib/features/list/list_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_work/features/add/add_page.dart';
 import 'package:todo_work/features/edit/edit_page.dart';
 import 'package:todo_work/features/list/views/cell_todo.dart';
+import 'package:todo_work/model/todo.dart';
 import 'package:todo_work/services/todo_service.dart';
 
 class ListPage extends StatefulWidget {
@@ -20,13 +22,18 @@ class ListPageState extends State<ListPage> {
     );
   }
 
-  void _moveToEditPost(BuildContext context, String id) {
+  void _moveToEditPost(BuildContext context, int id) {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => EditPage(todoId: id),
+        builder: (context) => EditPage(todoIndex: id),
       ),
     );
+  }
+
+  void _removeTodo(Todo todo) {
+    var todoService = context.read<TodoService>();
+    todoService.removeTodo(todo);
   }
 
   @override
@@ -38,21 +45,11 @@ class ListPageState extends State<ListPage> {
         title: const Text("List"),
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: todoService.todos.length,
-          itemBuilder: (context, index) {
-            var todo = todoService.todos[index];
-            return Dismissible(
-              key: Key(todo.id),
-              onDismissed: (direction) {
-                todoService.removeTodo(todo.id);
-              },
-              child: InkWell(
-                  onTap: () {
-                    _moveToEditPost(context, todo.id);
-                  },
-                  child: CellTodo(todo: todo)),
-            );
+        child: ValueListenableBuilder(
+          valueListenable: todoService.getTodoList(),
+          builder: (BuildContext context, Box<Todo> box, _) {
+            var todos = box.values.toList().cast<Todo>();
+            return _todoList(todos);
           },
         ),
       ),
@@ -61,6 +58,26 @@ class ListPageState extends State<ListPage> {
         tooltip: 'Add Todo',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  ListView _todoList(List<Todo> todos) {
+    return ListView.builder(
+      itemCount: todos.length,
+      itemBuilder: (context, index) {
+        var todo = todos[index];
+        return Dismissible(
+          key: Key(todo.id),
+          onDismissed: (direction) {
+            _removeTodo(todo);
+          },
+          child: InkWell(
+              onTap: () {
+                _moveToEditPost(context, index);
+              },
+              child: CellTodo(todo: todo)),
+        );
+      },
     );
   }
 }
